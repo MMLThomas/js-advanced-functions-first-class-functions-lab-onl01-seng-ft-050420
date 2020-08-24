644e6bacd772a43d6c20ff4f04b29d80c0f69138
michael thomas <michael.thomas2014@gmail.com>
2020-08-24 17:20:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 
 const returnFirstTwoDrivers = function(drivers){
-  let [driver1, drivr2] = drivers;
+  let [driver1, driver2] = drivers;
   return driver1, driver2
 }

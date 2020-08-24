12cd3b12388fea128154cad0b5bc401f5c58e59e
michael thomas <michael.thomas2014@gmail.com>
2020-08-24 17:25:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const returnFirstTwoDrivers = function(drivers){
   let firstTwo = [drivers[0], drivers[1]];
   return firstTwo
 }
+
+const returnLastTwoDrivers = function(drivers){
+  let lastTwo = [drivers[-1], drivers[-2]];
+  return lastTwo
+} 

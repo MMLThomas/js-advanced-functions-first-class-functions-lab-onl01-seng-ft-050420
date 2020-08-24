8d9b8106f7dc182af2cf940f2232d674cf7906b4
michael thomas <michael.thomas2014@gmail.com>
2020-08-24 17:21:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 
 const returnFirstTwoDrivers = function(drivers){
-  let arr;[driver1, driver2] = drivers;
-  return driver1, driver2
+  let firstTwo = [driver[0], driver[1]]
+  return firstTwo
 }
